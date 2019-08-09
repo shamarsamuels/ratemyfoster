c24433813ec3042cf196b5b15ddd02cc7c9af170
shamarsamuels <shamarsamuels@mec.science>
2019-08-08 21:30:46 -0400

    Update

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// const search = document.getElementById('search')
+
+// search.addEventListener('input' , el => {
+//     console.log(el.target.value)
+// })
+
+
+// $("#search").on('input', function(){
+//     console.log('Hi')
+//     $('#search_form').submit()
+// });
