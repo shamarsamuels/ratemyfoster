a157409fc0c7c76a64e0f4cb59d2e517367cb8d5
shamarsamuels <shamarsamuels@mec.science>
2019-08-14 14:10:55 -0400

    Update

--- scripts/family_page.js
+++ scripts/family_page.js
@@ -1,5 +1,7 @@
 $(document).ready(()=> {
     for(let i = 1;i <= 5;i++){
+        let clicked = false
+        let current_clicked = 0
         for(let x = 1;x <= 5;x++){
             let star = $(`#row_${i}_star_${x}`)
             star.hover(() => {
@@ -7,12 +9,23 @@ $(document).ready(()=> {
                     $(`#row_${i}_star_${y}`).attr('src', '/images/star2.png')
                 }
             }, () => {
-                for(let y=1; y <= x; y++){
-                    $(`#row_${i}_star_${y}`).attr('src', '/images/star.png')
+                if(!clicked || x > current_clicked){
+                    for(let y=current_clicked + 1; y <= x; y++){
+                        $(`#row_${i}_star_${y}`).attr('src', '/images/star.png')
+                    }
                 }
             })
             star.click(() => {
-                console.log('Clicked')
+                clicked = true
+                current_clicked = x
+                for(let y=1; y <= 5; y++){
+                    $(`#row_${i}_star_${y}`).attr('src', '/images/star.png')
+                }
+                for(let y=1; y <= x; y++){
+                    $(`#row_${i}_star_${y}`).attr('src', '/images/star2.png')
+                }
+                console.log(`#row_${i}_star_${x}`)
+                $(`#row_${i}_client_rating`).html(x + '.0')
             })
         }
     }
